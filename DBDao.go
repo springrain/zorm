b31505ddab3709a4701967f25e0e6fc37068e7fa
springrain <chunanyong@163.com>
2020-12-11 00:10:07 +0800

    修改注释

--- DBDao.go
+++ DBDao.go
@@ -41,7 +41,7 @@ const contextDBConnectionValueKey = wrapContextStringKey("contextDBConnectionVal
 
 //注释如果是 . 句号结尾,IDE的提示就截止了,注释结尾不要用 . 结束
 
-//allowBaseTypeMap 允许基础类型查询,用于查询单个基础类型字段,例如 select id from t_user
+//allowBaseTypeMap 允许基础类型查询,用于查询单个基础类型字段,例如 select id from t_user 查询返回的是字符串类型
 var allowBaseTypeMap = map[reflect.Kind]bool{
 	reflect.String: true,
 
@@ -138,7 +138,7 @@ Transaction 的示例代码
 // 如果入参ctx有dbConnection且没有事务,调用dbConnection.begin()开启事务并最后提交
 // 如果入参ctx有dbConnection且有事务,只使用不提交,有开启方提交事务
 // 但是如果遇到错误或者异常,虽然不是事务的开启方,也会回滚事务,让事务尽早回滚
-// 在多库的场景,手动获取dbConnection,然后帮定到一个新的context,传入进来
+// 在多库的场景,手动获取dbConnection,然后绑定到一个新的context,传入进来
 // 不要去掉匿名函数的context参数,因为如果Transaction的context中没有dbConnection,会新建一个context并放入dbConnection,此时的context指针已经变化,不能直接使用Transaction的context参数
 // bug(springrain)如果有大神修改了匿名函数内的参数名,例如改为ctx2,这样业务代码实际使用的是Transaction的context参数,如果为没有dbConnection,会抛异常,如果有dbConnection,实际就是一个对象.影响有限.也可以把匿名函数抽到外部
 // return的error如果不为nil,事务就会回滚
