c5047615cf8899f6004aca607913c79913a7ad6d
周阿星 <ZHOUAXING>
2021-02-17 13:44:28 +0800

    Update tip:    添加IEntity.go 英文注释.    添加Logger.go 英文注释.    添加Page.go 英文注释.

--- Logger.go
+++ Logger.go
@@ -7,19 +7,24 @@ import (
 
 func init() {
 	//设置默认的日志显示信息,显示文件和行号
+	//Set the default log display information, display file and line number.
 	log.SetFlags(log.Llongfile | log.LstdFlags)
 }
 
-//LogCallDepth 记录日志调用层级,用于定位到业务层代码
+//LogCallDepth： 记录日志调用层级,用于定位到业务层代码
+//Log Call Depth：Record the log call level, used to locate the business layer code.
 var LogCallDepth = 4
 
-//FuncLogError 记录error日志
+//FuncLogError： 记录error日志
+//FuncLogError：Record error log.
 var FuncLogError func(err error) = defaultLogError
 
-//FuncLogPanic 记录panic日志,默认使用ZormErrorLog实现
+//FuncLogPanic  记录panic日志,默认使用"ZormErrorLog"实现
+//FuncLogPanic：Record panic log, using "Zorm Error Log" by default.
 var FuncLogPanic func(err error) = defaultLogPanic
 
 //FuncPrintSQL 打印sql语句和参数
+//FuncPrintSQL：Print sql statement and parameters.
 var FuncPrintSQL func(sqlstr string, args []interface{}) = defaultPrintSQL
 
 func defaultLogError(err error) {
