2a285a1ad5f5fc659da206021d414ca5fd60e628
springrain <chunanyong@163.com>
2021-06-30 14:05:37 +0800

    修改随机数长度

--- dialect.go
+++ dialect.go
@@ -653,10 +653,11 @@ func generateStringID() string {
 	//获取9位数,前置补0,确保9位数
 	rand9 := fmt.Sprintf("%09d", randNum)
 
-	//获取纳秒 按照 年月日时分秒毫秒纳秒 拼接为字符串
-	pk := time.Now().Format("2006.01.02.15.04.05.000000")
+	//获取纳秒 按照 年月日时分秒毫秒微秒纳秒 拼接为长度23位的字符串
+	pk := time.Now().Format("2006.01.02.15.04.05.000000000")
 	pk = strings.ReplaceAll(pk, ".", "")
 
+	//23位字符串+9位随机数=32位字符串,这样的好处就是可以使用ID进行排序
 	pk = pk + rand9
 	return pk
 }
