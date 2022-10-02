dd0f4875323ceb907a6f6da28ce04c71a26c430a
springrain <chunanyong@163.com>
2022-10-02 08:20:13 +0800

    完善类型对照

--- DBDao.go
+++ DBDao.go
@@ -1094,21 +1094,21 @@ var queryMap = func(ctx context.Context, finder *Finder, page *Page) ([]map[stri
 			}
 
 			switch databaseTypeName {
-			case "CHAR", "NCHAR", "VARCHAR", "NVARCHAR", "VARCHAR2", "NVARCHAR2", "TINYTEXT", "MEDIUMTEXT", "TEXT", "NTEXT", "LONGTEXT", "LONG":
+			case "CHAR", "NCHAR", "VARCHAR", "NVARCHAR", "VARCHAR2", "NVARCHAR2", "TINYTEXT", "MEDIUMTEXT", "TEXT", "NTEXT", "LONGTEXT", "LONG", "CHARACTER", "MEMO":
 				values[i] = new(string)
-			case "INT", "INT4", "INTEGER", "SERIAL", "TINYINT", "BIT", "SMALLINT", "SMALLSERIAL", "INT2":
+			case "INT", "INT4", "INTEGER", "SERIAL", "SERIAL4", "SERIAL2", "TINYINT", "MEDIUMINT", "SMALLINT", "SMALLSERIAL", "INT2", "VARBIT", "AUTONUMBER":
 				values[i] = new(int)
-			case "BIGINT", "BIGSERIAL", "INT8":
+			case "BIGINT", "BIGSERIAL", "INT8", "SERIAL8":
 				values[i] = new(int64)
-			case "FLOAT", "REAL":
+			case "FLOAT", "REAL", "FLOAT4", "SINGLE":
 				values[i] = new(float32)
-			case "DOUBLE":
+			case "DOUBLE", "FLOAT8":
 				values[i] = new(float64)
 			case "DECIMAL", "NUMBER", "NUMERIC", "DEC":
 				values[i] = new(decimal.Decimal)
-			case "BOOLEAN", "BOOL":
+			case "BOOLEAN", "BOOL", "BIT":
 				values[i] = new(bool)
-			case "DATE", "TIME", "DATETIME", "TIMESTAMP", "TIMESTAMPTZ", "TIMETZ", "INTERVAL", "YEAR":
+			case "DATE", "TIME", "DATETIME", "TIMESTAMP", "TIMESTAMPTZ", "TIMETZ", "INTERVAL", "YEAR", "DATETIME2", "SMALLDATETIME", "DATETIMEOFFSET":
 				values[i] = new(time.Time)
 			default:
 				//不需要类型转换,正常赋值
